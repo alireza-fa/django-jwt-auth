9ac2f4bb9bc54bc87fe26626a9a29c2980338b39
alireza-fa <alirezafeyze44@gmail.com>
2023-10-30 15:39:38 +0330

    add: encrypt/decrypt option

---
+++ jwt_auth/encryption.py
@@ -0,0 +1,44 @@
+import base64
+import binascii
+from typing import ByteString
+
+from Crypto.Cipher import AES
+from Crypto.Cipher._mode_ecb import EcbMode
+from Crypto.Util.Padding import pad, unpad
+
+from .config import EN_KEY
+
+
+def get_new_cipher(key: ByteString) -> EcbMode:
+    return AES.new(key, AES.MODE_ECB)
+
+
+def ciphertext_encrypt(cipher: EcbMode, data: str) -> ByteString:
+    return cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
+
+
+def ciphertext_decode(encrypted_token: ByteString) -> bytes:
+    try:
+        cipher_decode = base64.b64decode(encrypted_token)
+    except Exception as e:
+        raise binascii.Error(e)
+    else:
+        return cipher_decode
+
+
+def encrypt(data: str, key: ByteString = EN_KEY) -> str:
+    cipher = get_new_cipher(key=key)
+    ciphertext = ciphertext_encrypt(cipher=cipher, data=data)
+    encrypted = base64.b64encode(ciphertext).decode('utf-8')
+    return encrypted
+
+
+def decrypt(encrypted: ByteString, key: ByteString = EN_KEY) -> str:
+    cipher = get_new_cipher(key=key)
+    ciphertext = ciphertext_decode(encrypted_token=encrypted)
+    try:
+        decrypted_token = unpad(cipher.decrypt(ciphertext), AES.block_size).decode('utf-8')
+    except Exception as e:
+        raise ValueError(e)
+    else:
+        return decrypted_token
