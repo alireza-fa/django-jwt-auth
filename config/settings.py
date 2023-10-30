ba7deb56e6ea7ab87a3168b87cac02b3d71a9b08
alireza-fa <alirezafeyze44@gmail.com>
2023-10-30 14:39:09 +0330

    add: django-simple-jwt package

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 
