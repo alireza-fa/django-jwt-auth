09baf2cac0b79e5db62e7a64aa28dea71bbf02ec
alireza-fa <alirezafeyze44@gmail.com>
2024-01-24 00:34:50 +0330

    remove some unuse import in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Permission, Group
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 
 from .managers import UserManager
