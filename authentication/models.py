77576d0323f41b2b31a2eb6eabf8037712c8178b
alireza-fa <alirezafeyze44@gmail.com>
2024-04-17 00:39:23 +0330

    add authentication/models UserAuth model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserAuth(models.Model):
+    ACCESS_TOKEN = 1
+    REFRESH_TOKEN = 2
+
+    TOKEN_TYPE_CHOICES = (
+        (ACCESS_TOKEN, "access token"),
+        (REFRESH_TOKEN, "refresh token")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_auths")
+    token_type = models.PositiveSmallIntegerField(choices=TOKEN_TYPE_CHOICES, verbose_name=_("token type"))
+    uuid = models.UUIDField(verbose_name=_("uuid"), unique=True)
+
+    class Meta:
+        verbose_name = _("UserAuth")
+        verbose_name_plural = _("UserAuths")
+
+    def __str__(self):
+        return f"{self.user} - token type(a = 1, r = 2): {self.token_type}"
