c2aa1e133c908cbe7244032c8708881a4ba9123b
alireza-fa <alirezafeyze44@gmail.com>
2024-04-18 06:04:53 +0330

    models tests

--- d_jwt_auth/models.py
+++ d_jwt_auth/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class UserAuthManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().select_related("user")
+
+
 class UserAuth(models.Model):
     ACCESS_TOKEN = 1
     REFRESH_TOKEN = 2
@@ -15,11 +21,14 @@ class UserAuth(models.Model):
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_auths")
     token_type = models.PositiveSmallIntegerField(choices=TOKEN_TYPE_CHOICES, verbose_name=_("token type"))
-    uuid = models.UUIDField(verbose_name=_("uuid"), unique=True)
+    uuid = models.UUIDField(verbose_name=_("uuid"), unique=True, db_index=True)
+
+    objects = UserAuthManager()
 
     class Meta:
         verbose_name = _("UserAuth")
         verbose_name_plural = _("UserAuths")
+        ordering = ("-id",)
 
     def __str__(self):
         return f"{self.user} - token type(a = 1, r = 2): {self.token_type} - {self.uuid}"
--- d_jwt_auth/tests/test_models.py
+++ d_jwt_auth/tests/test_models.py
@@ -1,5 +1,9 @@
+import uuid
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from ..models import UserAuth
 
 User = get_user_model()
 
@@ -7,4 +11,24 @@ User = get_user_model()
 class TestUserAuthModel(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user()
+        self.user = User.objects.create_user(
+            username="alireza",
+            password="password",
+            email="alirezafeyze44@gmail.com")
+
+    def test_user_auth_create(self):
+        UserAuth.objects.create(user=self.user, token_type=UserAuth.ACCESS_TOKEN, uuid=uuid.uuid4())
+        self.assertEqual(UserAuth.objects.count(), 1)
+
+    def test_ordering_user_auth(self):
+        for i in range(10):
+            UserAuth.objects.create(user=self.user, token_type=UserAuth.ACCESS_TOKEN, uuid=uuid.uuid4())
+
+        self.assertEqual(UserAuth.objects.first().id, 10)
+
+    def test_user_auth_query_num(self):
+        for i in range(100):
+            UserAuth.objects.create(user=self.user, token_type=UserAuth.ACCESS_TOKEN, uuid=uuid.uuid4())
+
+        with self.assertNumQueries(1):
+            [user_auth.user for user_auth in UserAuth.objects.all()]
