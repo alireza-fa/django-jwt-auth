39b1087ddcd0aef562ceb07fedf68ab0c6462c1c
alireza-fa <alirezafeyze44@gmail.com>
2024-04-19 10:32:08 +0330

    deleted authentication app

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,12 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local apps
-    # 'authentication.apps.AuthenticationConfig',
     'd_jwt_auth.apps.DJwtAuthConfig',
-    'api.apps.ApiConfig',
     # Third party apps
     'drf_spectacular',
-
 ]
 
 MIDDLEWARE = [
