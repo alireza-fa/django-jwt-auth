3fb58a390f5159ba6988cf6b4b4fb9d22a64bb86
alireza-fa <alirezafeyze44@gmail.com>
2024-04-19 22:09:20 +0330

    renamed django_jwt_auth to d_jwt_auth, add setup.py file

--- d_jwt_auth/apps.py
+++ d_jwt_auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoJwtAuth(AppConfig):
+class DJwtAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_jwt_auth'
+    name = 'd_jwt_auth'
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from io import open
+
+from setuptools import find_packages, setup
+
+
+def read(f):
+    with open(f, 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+setup(
+    name='d_jwt_auth',
+    version="0.0.1",
+    url='https://github.com/alireza-fa/django-jwt-auth',
+    license='MIT',
+    description='django-jwt-auth is an application for authenticating users with jwt in Django.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Alireza Feizi',
+    author_email='alirezafeyze44@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=["django>=3.2", "djangorestframework>=3.0", "djangorestframework-simplejwt>=5.0", "pycryptodome>=3.0", "model-bakery>=1.0"],
+    python_requires=">=3.9",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    project_urls={
+        'Source': 'https://github.com/alireza-fa/django-jwt-auth',
+    },
+)
